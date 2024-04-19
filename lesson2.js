e536e775e1142410c9bc8fc98cecefc10c6e652d
svitlana.zavialova <svzavyalova@gmail.com>
2024-04-19 21:25:45 +0300

    remove1

--- lesson2.js
+++
@@ -1,6 +0,0 @@
-let price = 10
-let itemName ="Cup"
-let messageToPrint1 = ("The price of your " +itemName + " is " +price + " dollars")
-let messageToPrint = (`The price of your ${itemName} is ${price} dollars`)
-console.log (messageToPrint)
-console.log (messageToPrint1)
