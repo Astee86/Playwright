00db718697b3b762967b8c206f6c9eb60b32a866
svitlana.zavialova <svzavyalova@gmail.com>
2024-04-19 21:36:02 +0300

    remove4

--- lesson5.js
+++
@@ -1,5 +0,0 @@
-let ageIsMoreThan18 = true
-let isUSCitizen = true
-
-let eligibilityForDrivingLicense = ageIsMoreThan18 && isUSCitizen
-console.log("This custome is eligible for the Driving License: " +eligibilityForDrivingLicense)
--- lesson7.js
+++
@@ -1,9 +0,0 @@
-for (i=0; i < 5; i++) {
-    console.log("Let me die " + i)
-}
-
-let fruits = ["Apple", "Orange", "Banana"]
-for (fruit of fruits) {
-    console.log(fruit)
-
-}
